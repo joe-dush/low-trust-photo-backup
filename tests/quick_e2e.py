0123b02c1b09ca29f3b975122154aee3afed2700
Joe Unwin <joeunwin55@gmail.com>
2025-10-03 22:38:56 +0100

    grouping broken. need a snapshot dataclass to make this easier

--- tests/quick_e2e.py
+++ tests/quick_e2e.py
@@ -1,10 +1,15 @@
 import os
 from pathlib import Path
 from low_trust_photo_backup.client.tracking import detect_created_and_modified_files
+from low_trust_photo_backup.client.preparation import group_files_by_size
 
 TEST_PATH_STR = "C:\\Users\\joeun\\02_Photos\\iPhone12 Monthly"
 # TEST_PATH_STR = os.path.join("tests", "test_dir")
 
 
 c_u_files = detect_created_and_modified_files(Path(TEST_PATH_STR))
-print(c_u_files)
+# print(c_u_files)
+
+groups = group_files_by_size(c_u_files, max_size_gb=2)
+for group in groups:
+    print(f"{len(group)=}")
